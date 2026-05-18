76ea173165b94b392bdae57ae1ebc756125b0769
iy88 <1845767798@qq.com>
2026-05-18 16:22:09 +0800

    chore: using 6768 as default port

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ config_name = os.environ.get('FLASK_ENV', 'development')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run(debug=app.config.get('DEBUG', True), host='0.0.0.0', port=6767)
+    app.run(debug=app.config.get('DEBUG', True), host='0.0.0.0', port=6768)
